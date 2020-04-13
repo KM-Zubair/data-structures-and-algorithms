6157aa6520d405dd65a3eae4d85e9558b1c7cae6
KM Zubair <47664890+KM-Zubair@users.noreply.github.com>
2020-04-13 10:39:04 +0800

    Create Solution.py

---
+++ HackerRank/sWAP cASE/Solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  13 10:38:39 2020
+
+@author: KM Zubair 
+"""
+
+def swap_case(s):
+    return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
